80d83c1eec7c200222f1b368ef89cde4ee6ef35d
Rick Mak <rick.mak@gmail.com>
2016-11-23 21:27:36 +0800

    Add node project scaffolding

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import skygearChat from '../lib';
+
+describe('skygear-chat', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
