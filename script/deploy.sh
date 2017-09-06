349e1c5fd3293173177e0b03dd8d67ea105ad2b2
Ho Wa <howa@oursky.com>
2017-09-06 16:41:04 +0800

    version updated

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,7 +5,7 @@ fi
 
 if [ "$TRAVIS_TAG" == "latest" ]; then
   generate-js-doc --pwd $PWD
-  publish-doc --platform js --pwd $PWD  --doc-dir $PWD/esdoc --bucket 'docs.skygear.io' --prefix '/js/chat/reference' --version 'latest' --distribution-id E31J8XF8IPV2V
+  publish-doc --platform js --pwd $PWD  --doc-dir $PWD/esdoc --bucket 'docs.skygear.io' --prefix '/js/chat/reference' --version $TRAVIS_TAG --distribution-id E31J8XF8IPV2V
   npm run deploy-latest
 fi
 
