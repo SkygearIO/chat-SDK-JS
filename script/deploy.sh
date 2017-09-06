c2f0bc0ac64b66bd0f92963b1b9ff480115294d8
Ho Wa <howa@oursky.com>
2017-09-06 15:34:14 +0800

    Deploy script updated

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,8 +3,9 @@ if [ -n "$TRAVIS_TAG" ]; then
   npm run deploy
 fi
 
-if [ "$TRAVIS_BRANCH" == "latest" ]; then
-  npm run deploy-latest
+if [ "$TRAVIS_BRANCH" == "update-doc" ]; then
+  generate-js-doc --pwd $PWD
+  publish-doc --platform js --pwd $PWD  --doc-dir $PWD/esdoc --bucket 'docs.skygear.io' --prefix '/chat-js/reference' --version 'latest' --distribution-id E31J8XF8IPV2V
 fi
 
 # Notify doc.esdoc.org to regenerate esdoc
