f6aea87558977c48eae3294066bc3afc32672f33
Carmen <carmen@oursky.com>
2018-12-10 19:22:57 +0800

    Remove not used esdoc deployment

--- script/deploy.sh
+++ script/deploy.sh
@@ -8,11 +8,3 @@ if [ "$TRAVIS_TAG" == "latest" ]; then
   publish-doc --platform js --pwd $PWD  --doc-dir $PWD/esdoc --bucket 'docs.skygear.io' --prefix '/js/chat/reference' --version $TRAVIS_TAG --distribution-id E31J8XF8IPV2V
   npm run deploy-latest
 fi
-
-# Notify doc.esdoc.org to regenerate esdoc
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  curl 'https://doc.esdoc.org/api/create' \
-    -XPOST \
-    -H 'Content-Type: application/x-www-form-urlencoded' \
-    --data 'gitUrl=git%40github.com%3Askygeario%2Fchat-SDK-JS.git'
-fi
