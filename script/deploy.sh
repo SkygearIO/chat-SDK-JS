f50b1c262cb82e26cd1a4838bb76c75d8bcc7b3a
Carmen <carmen@oursky.com>
2018-12-10 19:23:51 +0800

    Run doc generation on all branches and tags

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,8 +3,11 @@ if [ -n "$TRAVIS_TAG" ]; then
   npm run deploy
 fi
 
+# generate doc for all tags and branches
+generate-js-doc --pwd $PWD
+
+# only deploy on latest tag
 if [ "$TRAVIS_TAG" == "latest" ]; then
-  generate-js-doc --pwd $PWD
   publish-doc --platform js --pwd $PWD  --doc-dir $PWD/esdoc --bucket 'docs.skygear.io' --prefix '/js/chat/reference' --version $TRAVIS_TAG --distribution-id E31J8XF8IPV2V
   npm run deploy-latest
 fi
